82821221ea12772d71a4ef3dcc667b6705ec6bc0
Shadow <153516052+ShadowUR0@users.noreply.github.com>
2026-09-04 21:41:38 +0300

    feat(ui): glass-wrap standalone create route

--- ui/flutter/lib/app/modules/root/views/root_view.dart
+++ ui/flutter/lib/app/modules/root/views/root_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../../theme/liquid_glass.dart';
 import '../../../routes/app_pages.dart';
 import '../controllers/root_controller.dart';
 
@@ -11,13 +12,29 @@ class RootView extends GetView<RootController> {
   Widget build(BuildContext context) {
     return GetRouterOutlet.builder(
       builder: (context, delegate, current) {
-        return GetRouterOutlet(
-          initialRoute: Routes.HOME,
-          // anchorRoute: '/',
-          // filterPages: (afterAnchor) {
-          //   return afterAnchor.take(1);
-          // },
-        );
+        final outlet = GetRouterOutlet(initialRoute: Routes.HOME);
+        final path = current?.uri.path;
+
+        if (path == Routes.CREATE) {
+          return SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: LiquidGlassSurface(
+                radius: 30,
+                blur: 18,
+                tint: Theme.of(context).brightness == Brightness.dark
+                    ? const Color(0x34151F1C)
+                    : const Color(0x42FFFFFF),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(29),
+                  child: outlet,
+                ),
+              ),
+            ),
+          );
+        }
+
+        return outlet;
       },
     );
   }
