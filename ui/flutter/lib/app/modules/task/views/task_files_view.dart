267813b9367f1d4aa9dca00346589be9ea069665
Shadow <153516052+ShadowUR0@users.noreply.github.com>
2026-09-05 00:28:01 +0300

    test(ci): sync liquid task file breadcrumb

--- ui/flutter/lib/app/modules/task/views/task_files_view.dart
+++ ui/flutter/lib/app/modules/task/views/task_files_view.dart
@@ -6,6 +6,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../api/api.dart' as api;
+import '../../../../theme/liquid_glass.dart';
 import '../../../../util/browser_download/browser_download.dart';
 import '../../../../util/util.dart';
 import '../../../views/breadcrumb_view.dart';
@@ -18,29 +19,37 @@ class TaskFilesView extends GetView<TaskFilesController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () => Get.rootDelegate.popRoute()),
-          // actions: [],
-          title: Obx(() => Text(controller.task.value?.meta.res?.name ?? "")),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Get.rootDelegate.popRoute(),
         ),
-        body: Obx(() {
-          final fileList = controller.fileList;
-          final breadcrumbItems = ["/"];
-          if (fileList.isNotEmpty) {
-            final file = fileList.first;
-            final path = file.path.substring(1);
-            if (path.isNotEmpty) {
-              final pathArr = path.split("/");
-              for (int i = 0; i < pathArr.length; i++) {
-                breadcrumbItems.add(pathArr[i]);
-              }
+        title: Obx(() => Text(controller.task.value?.meta.res?.name ?? "")),
+      ),
+      body: Obx(() {
+        final fileList = controller.fileList;
+        final breadcrumbItems = ["/"];
+        if (fileList.isNotEmpty) {
+          final file = fileList.first;
+          final path = file.path.substring(1);
+          if (path.isNotEmpty) {
+            final pathArr = path.split("/");
+            for (int i = 0; i < pathArr.length; i++) {
+              breadcrumbItems.add(pathArr[i]);
             }
           }
-          return Column(
-            children: [
-              Breadcrumb(
+        }
+
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 12, 12, 8),
+              child: LiquidGlassSurface(
+                radius: 22,
+                blur: 14,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                child: Breadcrumb(
                   items: breadcrumbItems,
                   onItemTap: (index) {
                     final targetDirArr = <String>[];
@@ -49,84 +58,90 @@ class TaskFilesView extends GetView<TaskFilesController> {
                     }
                     controller
                         .toDir(targetDirArr.join("/").replaceFirst('//', '/'));
-                  }).paddingOnly(left: 16, top: 16, bottom: 8),
-              Expanded(
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    final meta = controller.task.value!.meta;
-                    final file = fileList[index];
-                    // if resource is single file, use opts.name as file name
-                    final realFileName = meta.res!.name.isEmpty
-                        ? (meta.opts.name.isEmpty ? file.name : meta.opts.name)
-                        : "";
-                    final fileRelativePath = file.filePath(realFileName);
-                    final filePath = Util.safePathJoin(
-                        [meta.opts.path, meta.res!.name, fileRelativePath]);
-                    final fileName = basename(filePath);
-                    return ListTile(
-                      leading:
-                          Icon(fileIcon(fileName, isFolder: file.isDirectory)),
-                      title: Text(fileName),
-                      subtitle: file.isDirectory
-                          ? Text('items'.trParams({
-                              'count': controller
-                                  .dirItemCount(file.fullPath)
-                                  .toString()
-                            }))
-                          : Text(Util.fmtByte(file.size)),
-                      trailing: file.isDirectory
-                          ? null
-                          : SizedBox(
-                              width: 100,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: Util.isWeb()
-                                    ? () {
-                                        final accessUrl = api.join(
-                                            "/fs/tasks/${controller.task.value!.id}$fileRelativePath");
-                                        return [
-                                          IconButton(
-                                              icon:
-                                                  const Icon(Icons.open_in_new),
-                                              onPressed: () {
-                                                launchUrl(Uri.parse(accessUrl),
-                                                    webOnlyWindowName:
-                                                        "_blank");
-                                              }),
-                                          IconButton(
-                                              icon: const Icon(Icons.download),
-                                              onPressed: () {
-                                                download(accessUrl, fileName);
-                                              })
-                                        ];
-                                      }()
-                                    : [
-                                        IconButton(
-                                            icon: const Icon(Icons.open_in_new),
-                                            onPressed: () async {
-                                              await OpenFilex.open(filePath);
-                                            }),
-                                        IconButton(
-                                            icon: const Icon(Icons.share),
-                                            onPressed: () {
-                                              final xfile = XFile(filePath);
-                                              Share.shareXFiles([xfile]);
-                                            })
-                                      ],
-                              ),
-                            ),
-                      onTap: () {
-                        if (file.isDirectory) {
-                          controller.toDir(file.fullPath);
-                        }
-                      },
-                    );
                   },
-                  itemCount: controller.fileList.length,
                 ),
-              )
-            ],
-          );
-        }));
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  final meta = controller.task.value!.meta;
+                  final file = fileList[index];
+                  // if resource is single file, use opts.name as file name
+                  final realFileName = meta.res!.name.isEmpty
+                      ? (meta.opts.name.isEmpty ? file.name : meta.opts.name)
+                      : "";
+                  final fileRelativePath = file.filePath(realFileName);
+                  final filePath = Util.safePathJoin(
+                      [meta.opts.path, meta.res!.name, fileRelativePath]);
+                  final fileName = basename(filePath);
+                  return ListTile(
+                    leading:
+                        Icon(fileIcon(fileName, isFolder: file.isDirectory)),
+                    title: Text(fileName),
+                    subtitle: file.isDirectory
+                        ? Text('items'.trParams({
+                            'count': controller
+                                .dirItemCount(file.fullPath)
+                                .toString()
+                          }))
+                        : Text(Util.fmtByte(file.size)),
+                    trailing: file.isDirectory
+                        ? null
+                        : SizedBox(
+                            width: 100,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: Util.isWeb()
+                                  ? () {
+                                      final accessUrl = api.join(
+                                          "/fs/tasks/${controller.task.value!.id}$fileRelativePath");
+                                      return [
+                                        IconButton(
+                                          icon: const Icon(Icons.open_in_new),
+                                          onPressed: () {
+                                            launchUrl(Uri.parse(accessUrl),
+                                                webOnlyWindowName: "_blank");
+                                          },
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(Icons.download),
+                                          onPressed: () {
+                                            download(accessUrl, fileName);
+                                          },
+                                        )
+                                      ];
+                                    }()
+                                  : [
+                                      IconButton(
+                                        icon: const Icon(Icons.open_in_new),
+                                        onPressed: () async {
+                                          await OpenFilex.open(filePath);
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Icons.share),
+                                        onPressed: () {
+                                          final xfile = XFile(filePath);
+                                          Share.shareXFiles([xfile]);
+                                        },
+                                      )
+                                    ],
+                            ),
+                          ),
+                    onTap: () {
+                      if (file.isDirectory) {
+                        controller.toDir(file.fullPath);
+                      }
+                    },
+                  );
+                },
+                itemCount: controller.fileList.length,
+              ),
+            )
+          ],
+        );
+      }),
+    );
   }
 }
